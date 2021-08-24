49626bb4ed1ec7ac1998f3faad56f881267577d4
Illya Cherkasov <cherkasov.i.a@gmail.com>
2021-08-24 23:04:48 +0300

    update

--- Readme.md
+++ Readme.md
@@ -1,3 +1,26 @@
+# Github:
+## https://github.com/icherkasov/lecture7
+### For tasks 1.3 and 2 please use browser (index.html) because of alerts and prompts
+<br></br>
+# Task 1.3
+## Вывести в консоль числа кратные k, в диапазоне от 1 до n
+
+<br></br>
+
+# Task 2
+
+```
+ В ваш подъезд вьехали новые жильцы, которые привезли с собой тараканов.
+ Насекомые в поисках еды ползут по вентиляционной шахте.
+ За час они поднимаются на 1м, но сразу после этого теряют равновесие и скатываются вниз на 0.5м
+ Вопрос: сколько времени у вас есть на покупку ловушек для тараканов, если расстояние от вас до соседей 5м.
+ (*) Напишите функцию, которая будет решать эту задачу в общем виде, для любых (speed, slowdown, mine),
+ где mine - это высота шахты, speed - скорость таракана за час, slowdown - расстояние падения из-за усталости
+```
+
+<br></br>
+# CodeWars tasks
+
 ## [Return the Missing Element]
 ### (not using indexOf(i))
 ### https://www.codewars.com/kata/reviews/52995cff9ce954dc50000a86/groups/61241155cafa9100011b659a
@@ -13,6 +36,7 @@
 
 ## [Take a Ten Minute Walk]
 ### Can't save it to codewars - some bug after submit
+#### task: https://www.codewars.com/kata/54da539698b8a2ad76000228/train/javascript
 ```
  function isValidWalk(walk) {
     let xPos = 0;
---
+++ tasks.js
@@ -0,0 +1,69 @@
+function printNumbers(number, mult) {
+    for (let i = 1; i <= number; i++) {
+        if (i % mult === 0) {
+            console.log(i);
+        }
+    }
+    alert('Numbers printed to console');
+}
+
+function runTask1_3() {
+    let number = prompt('Insert max value');
+    if (isNaN(number)) {
+        alert(`Value ${number} must be number`);
+        return;
+    }
+    let mult = prompt('Insert multiplicator');
+    if (isNaN(mult)) {
+        alert(`Value ${mult} must be number`);
+        return;
+    }
+
+    printNumbers(number, mult);
+}
+
+
+function calculateTimeLeft(mine, speed, slowdown) {
+    //так как минуты не учитываем - если длина шахты меньше скорости - считаем что времени  0 ( то есть меньше 1 часа)
+    if (mine <= speed) {
+        return 0;
+    }
+
+    let time = 0;
+    let distance = 0;
+    do {
+        //так как тараканы скатываются после того как преодолели какой-то путь,
+        //выношу скатывание перед просчетом расстояния так как нет смысла скатываться когда добрались до вершиныб
+        //с поправкой на первую итерацию так как считаем что тараканы на чем то стоят и скатываться им некуда
+        if (distance !== 0) {
+            distance -= slowdown;
+        }
+
+        distance += speed;
+        time++;
+
+    } while (distance < mine);
+
+    return time;
+}
+
+function runTask2() {
+    let mine = prompt('Insert distance');
+    if (isNaN(mine)) {
+        alert(`Value ${mine} must be number`);
+        return;
+    }
+    let speed = prompt('Insert speed');
+    if (isNaN(speed)) {
+        alert(`Value ${speed} must be number`);
+        return;
+    }
+    let slowdown = prompt('Insert slowdown');
+    if (isNaN(slowdown)) {
+        alert(`Value ${slowdown} must be number`);
+        return;
+    }
+
+    alert('You still have: ' + calculateTimeLeft(parseInt(mine), parseFloat(speed), parseFloat(slowdown)) + ' hours to get ready');
+}
+
